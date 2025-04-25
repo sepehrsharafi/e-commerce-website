5424a5ce2182a1726c704e16c154acf4edb40c19
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-25 22:12:33 +0330

    Chore: made the hero section image more optimized

--- src/app/ui/hero-section.jsx
+++ src/app/ui/hero-section.jsx
@@ -4,21 +4,22 @@ import HeroItem from "./hero-item";
 export default function HeroSection() {
   return (
     <section className="flex flex-col gap-4 mb-6">
-      <div className=" mx-4 flex flex-col justify-start gap-8 h-[calc(100vh-80px)] text-white rounded-3xl overflow-clip p-6 relative">
+      <div className="bg-[#79716d] mx-4 flex flex-col justify-start gap-8 h-[calc(100vh-80px)] text-white rounded-3xl overflow-clip p-6 relative">
         <h1 className="text-6xl leading-[70px]">
           Color of <br /> Summer <br /> Outfit
         </h1>
         <h2 className="text-base">
           100+ Collections for your outfit inspirations in this summer
         </h2>
-        <button className="py-4 bg-black rounded-full text-center w-full text-sm leading-normal tracking-wide">
+        <button className="py-4 z-10 bg-black rounded-full text-center w-full text-sm leading-normal tracking-wide">
           VIEW COLLECTIONS
         </button>
         <Image
-          className="absolute -z-10 w-full h-full object-cover object-left"
-          src={"/images/3071b1fc729091cd0452fb9d0b89106ceec16368.png"}
+          className="absolute top-0 left-0 w-full h-full object-contain object-bottom-right"
+          src={"/images/5e143183ca0df25c3d226a223269e70541e09760.png"}
           alt="image"
-          fill
+          width={1100}
+          height={1100}
         />
       </div>
       <div className="flex flex-row gap-4 overflow-auto px-4">
